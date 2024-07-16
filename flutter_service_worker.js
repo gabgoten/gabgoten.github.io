ee557bb3e939dbd4c78ae39a15bd18807b2edef8
Gabriel Rodrigues <150351559+gabgoten@users.noreply.github.com>
2024-07-15 23:17:38 -0300

    Ver 2.0.8

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -67,9 +67,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "828172f30720a53f4af8659de8b7c630
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a04e97ca949bb1e07acb40eaeacfbc22",
-"/": "a04e97ca949bb1e07acb40eaeacfbc22",
-"main.dart.js": "87652537d9238aa58582c5f1b318c591",
+"index.html": "e822f8a497e82e2f75968a136c93cc34",
+"/": "e822f8a497e82e2f75968a136c93cc34",
+"main.dart.js": "ef867f1ec584a7289ca6032487b9a9cb",
 "manifest.json": "198c2bfa7f0cefe0afbb15dd7393e3ec",
 "version.json": "022c20f31ce93600fb748dc27b22cb4b"};
 // The application shell files that are downloaded before a service worker can
