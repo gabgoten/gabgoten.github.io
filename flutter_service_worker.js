a0bb14bf67f33fd3ce5ff2b5113e73d6cf8c0461
Gabriel Rodrigues <150351559+gabgoten@users.noreply.github.com>
2024-08-28 16:46:05 -0300

    Ver 2.2.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,7 +8,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "828172f30720a53f4af8659de8b7c630
 "assets/AssetManifest.json": "741e35a477a1d0e89269c7d4bc5c8a68",
 "assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/fonts/MaterialIcons-Regular.otf": "1056cb5c963429e22d3ca0d680212759",
-"assets/NOTICES": "43227f977714aa4cce187e045fc63353",
+"assets/NOTICES": "df28bacaed5eb17e9d369acc676127b1",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/packages/flex_color_picker/assets/opacity.png": "49c4f3bcb1b25364bb4c255edcaaf5b2",
 "assets/packages/flutter_signin_button/assets/logos/2.0x/facebook_new.png": "83bf0093719b2db2b16e2839aee1069f",
@@ -67,9 +67,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "828172f30720a53f4af8659de8b7c630
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2b4fdb02ba70e48483ce07cc62351a8b",
-"/": "2b4fdb02ba70e48483ce07cc62351a8b",
-"main.dart.js": "c49e003583e2cb106442699a1777550c",
+"index.html": "605a2a5bf861c6dab874848dd0b8f0dd",
+"/": "605a2a5bf861c6dab874848dd0b8f0dd",
+"main.dart.js": "5d846d9ce561d029e7b370ed00efb24b",
 "manifest.json": "198c2bfa7f0cefe0afbb15dd7393e3ec",
 "version.json": "022c20f31ce93600fb748dc27b22cb4b"};
 // The application shell files that are downloaded before a service worker can
